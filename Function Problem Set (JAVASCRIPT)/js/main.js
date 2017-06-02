c819a036faaebcb8891b4f94979073b2018c2915
Marko Peric <marcko.peric@gmail.com>
2017-06-02 15:08:55 +0200

    first add

--- Function Problem Set (JAVASCRIPT)/js/main.js
+++ Function Problem Set (JAVASCRIPT)/js/main.js
@@ -16,17 +16,12 @@ function isEven(num){
 }
 
 function factorial(num){
-  //define a result variable
   var result = 1;
-  //calculate factorial and store value in result
   for(var i = 2; i <= num; i++){
   	result *= i;
   }
-  //return the result variable
   return result;
 }
-
-// factorial(4) 4 x 3 x 2 x 1
 
 
 function kebabToSnake(str) {
--- Guessing Game (JAVASCRIPT)/js/main.js
+++ Guessing Game (JAVASCRIPT)/js/main.js
@@ -1,15 +1,12 @@
-//create secretNumber
 var secretNumber = 4;
 
-//ask user for guess
 var stringGuess = prompt("Guess a number");
 var guess = Number(stringGuess);
 
-//check if guess is right
 if(guess === secretNumber) {
 	alert("YOU GOT IT RIGHT!");
 }
-//check if guess is higher
+
 else if(guess > secretNumber){
 	alert("Too high.  Guess again!");
 }
@@ -17,4 +14,3 @@ else if(guess > secretNumber){
 else {
 	alert("Too low.  Guess again!");
 }
-//otherwise, check if lower
